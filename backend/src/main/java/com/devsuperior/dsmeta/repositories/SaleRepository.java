e713cdcc891572f73e2a93e252cdf238ae965d1f
andermsilva <67654030+andermsilva@users.noreply.github.com>
2023-02-26 21:54:50 -0300

    Update formating imports SaleRepository.java

--- backend/src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ backend/src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -2,7 +2,6 @@ package com.devsuperior.dsmeta.repositories;
 
 import java.time.LocalDate;
 import java.util.List;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
